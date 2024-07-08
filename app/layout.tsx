fad68898e0aa49f5047bde5295f6b78a25a44c99
FrekiManagarm <mathchambaud@icloud.com>
2024-07-08 17:53:05 +0200

    wip refacto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
-import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import ActiveSectionContextProvider from "@/context/active-section-context";
-import Footer from "@/components/footer";
-import ThemeSwitch from "@/components/theme-switch";
-import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
 import { Metadata } from "next";
 import Providers from "./providers";
+import { cn } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Mathieu Chambaud',
-  description: 'Mathieu Chambaud est un jeune développeur diplômé',
+  description: 'Mathieu Chambaud',
 }
 
 export default function RootLayout({
@@ -22,26 +18,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={cn(
+          inter.className,
+          "flex antialiased h-screen overflow-hidden bg-gray-100"
+        )}
       >
-
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            <Header />
-            <Providers>
+        <Providers>
+          <div className="lg:pl-2 lg:pt-2 bg-gray-100 flex-1 overflow-y-auto">
+            <div className="flex-1 bg-white min-h-screen lg:rounded-tl-xl border border-transparent lg:border-neutral-200 overflow-y-auto">
               {children}
-            </Providers>
-            <Footer />
-
-            <Toaster position="bottom-center" />
-            <ThemeSwitch />
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
+            </div>
+          </div>
+        </Providers>
+        <Toaster position="bottom-center" />
       </body>
     </html>
   )
